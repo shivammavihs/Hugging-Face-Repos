21dd1012922f51deddf4daec367eb1b41a628ff9
shivam patel <shivammavihspatel97@gmail.com>
2023-02-06 13:32:36 +0530

    git commit

--- htmls.py
+++
@@ -1,8 +0,0 @@
-header= '''
-<div class="header">
-  <h1>ShopWisely</h1>
-  <p>Amazon Product Analyst</p>
-</div>'''  
-
-horizontal_line = '''<hr style="border: 0;height: 3px;background-image: linear-gradient(to right, rgba(0, 0, 0, 0), #610014, rgba(0, 0, 0, 0));" />'''
-
